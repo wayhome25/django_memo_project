f00370f4f299225a5a8178c9f7097e5c416ddd4d
Lee hyunjoo <siwabada@gmail.com>
2017-03-02 00:25:48 +0900

    add model class, url pattern, admin

--- memo_app/models.py
+++ memo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Memos(models.Model):
+    name = models.CharField(max_length = 20, db_column='이름')
+    title = models.CharField(max_length = 50, db_column='제목')
+    text = models.TextField(max_length = 150, db_column='내용', help_text='메모 내용은 150자 이내로 입력 가능합니다.')
+    update_date = models.DateTimeField()
+    Priority = models.BooleanField(db_column='중요')
+
+    def __str__(self):
+        return '%s by %s' % (self.title, self.name)
